d257fc4cd49c50838e99f8327149e1bf83007a19
Milan <milanvpage@gmail.com>
2021-09-24 12:34:28 -0700

    commit tree 9d50bc0910fc8cb16cb3938849bf6a5e9cbe2c78

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your functions here
+function map(origArr, funcOper) {
+    const newArr = [];
+    for(let i = 0; i < origArr.length; i++) {
+        newArr.push(funcOper(origArr[i]));
+    };
+    return newArr;
+};
+
+function reduce(origArr, funcOper, startPt) {
+    let total;
+    let i; 
+
+    if (!!startPt) {
+        total = startPt;
+        i = 0;
+    } else {
+        total = origArr[0];
+        i = 1;
+    };
+
+    for( ; i < origArr.length; i++) {
+        total = funcOper(origArr[i], total);
+    };
+    return total;
+} 
